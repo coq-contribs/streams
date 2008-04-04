846b6548a80cdaf98038957d795e16d214e7a60c
notin <notin@0cf17b13-060f-0410-b1b1-c666bec9822a>
2008-04-04 16:33:04 +0000

    Harmonisation des noms des contributions: première passe
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/coq-contribs/trunk@749 0cf17b13-060f-0410-b1b1-c666bec9822a

---
+++ Eratosthene.v
@@ -0,0 +1,263 @@
+(* This program is free software; you can redistribute it and/or      *)
+(* modify it under the terms of the GNU Lesser General Public License *)
+(* as published by the Free Software Foundation; either version 2.1   *)
+(* of the License, or (at your option) any later version.             *)
+(*                                                                    *)
+(* This program is distributed in the hope that it will be useful,    *)
+(* but WITHOUT ANY WARRANTY; without even the implied warranty of     *)
+(* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      *)
+(* GNU General Public License for more details.                       *)
+(*                                                                    *)
+(* You should have received a copy of the GNU Lesser General Public   *)
+(* License along with this program; if not, write to the Free         *)
+(* Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA *)
+(* 02110-1301 USA                                                     *)
+
+
+(* Contribution to the Coq Library   V6.3 (July 1999)                    *)
+(****************************************************************************)
+(*                 The Calculus of Inductive Constructions                  *)
+(*                                                                          *)
+(*                                Projet Coq                                *)
+(*                                                                          *)
+(*                     INRIA                        ENS-CNRS                *)
+(*              Rocquencourt                        Lyon                    *)
+(*                                                                          *)
+(*                                 Coq V6.1                                 *)
+(*                               Oct 1st 1996                               *)
+(*                                                                          *)
+(****************************************************************************)
+(*                              Eratosthene.v                               *)
+(****************************************************************************)
+
+(****************************************************************************)
+(****************************************************************************)
+(*									    *)
+(*            SPECIFICATION  ET PREUVE DU CRIBLE D'ERATOSTHENE 		    *)
+(*                     DANS LE CALCUL DES CONSTRUCTIONS  		    *)
+(*            Francois Leclerc - ENS Lyon - Juin 91                         *)
+(*									    *)
+(****************************************************************************)
+(****************************************************************************)
+
+(*******************************************)
+(* Arithmetical lemmas, euclidean division *)
+(*******************************************)
+
+Require Import Arith.
+Require Import Peano_dec.
+Require Import Euclid.
+
+Definition div (b a : nat) : Prop := exists q : nat, a = q * b.
+
+(* Proof of decidability of div *)
+Lemma div_mod_O :
+ forall a b r q : nat, a = q * b + r -> div b a -> b > r -> 0 = r.
+simple induction 2; intros q' Hq'.
+replace r with ((q' - q) * b); intros.
+elim (mult_O_le b (q' - q)); intros.
+rewrite H2; auto with v62.
+absurd (b <= (q' - q) * b); auto with v62.
+rewrite mult_minus_distr_r.
+symmetry  in |- *; apply plus_minus.
+elim H; auto with v62.
+Qed.
+
+Lemma div_dec : forall p q : nat, 0 < p -> {div p q} + {~ div p q}.
+intros; elim modulo with p q; intros; auto with v62.
+elim (eq_nat_dec 0 x); intro y0.
+left; elim p0; intros q0 Hq0.
+red in |- *; exists q0.
+elim Hq0; intros.
+rewrite H0.
+elim y0; auto with v62.
+right; red in |- *; intro; absurd (0 = x); trivial with v62.
+elim p0; simple induction 1; intros.
+apply div_mod_O with q p x0; auto with v62.
+Qed.
+
+(***************************************************************************)
+(* DEFINITION INDUCTIVE							   *)
+(* divinf(p,q): il existe r entier, 1<r<=q, tel que r divise p 		   *)
+(***************************************************************************)
+
+Inductive divinf (p q : nat) : Prop :=
+    Bs : forall r : nat, 2 <= r -> r <= q -> div r p -> divinf p q.
+
+
+(***************************************************************************)
+(* DEFINITION INDUCTIVE							   *)
+(* nondivinf(p,1) :pas de diviseur de p inferieur ou egal a 1		   *)
+(* si nondivinf(p,q) et si Sq ne divise pas p alors nondivinf(p,Sq)	   *)
+(* si nondivinf(p,q) et si existe 1<r<q, r divise Sq alors nondivinf(p,Sq) *) 	
+(***************************************************************************)
+
+Inductive nondivinf (p : nat) : nat -> Prop :=
+  | Base : nondivinf p 1
+  | Step1 :
+      forall q : nat, nondivinf p q -> ~ div (S q) p -> nondivinf p (S q)
+  | Step2 :
+      forall q : nat, nondivinf p q -> divinf (S q) q -> nondivinf p (S q).
+
+Hint Resolve Base Step1 Step2.
+
+(***************************************************************************)
+(* DEFINITION INDUCTIVE "a la CAML" (type concret mais dependant)	   *)
+(* natext(p,n) s'il n'existe pas de diviseur de n <= p 			   *)
+(* natext(p,n) s'il existe un diviseur de n <= p			   *)
+(***************************************************************************)
+
+Inductive natext (p n : nat) : Set :=
+  | Nondiv : nondivinf n p -> natext p n
+  | Div : divinf n p -> natext p n.
+
+Hint Resolve Nondiv Div.
+
+(***************************************************************************)
+(* DEFINITION 								   *)
+(* pour n et p donne, il existe P de type nat->Set telle que 		   *)
+(* - on ait une preuve de P(n)						   *)
+(* - pour tout q une preuve de P(q) donne une preuve de de P(Sq)	   *)
+(* - pour tout q une preuve de P(q) donne une preuve de natext(p,q)	   *)
+(*   i.e une preuve de l'existence ou non d'un diviseur de q dans [1...p]  *) 
+(***************************************************************************)
+Definition Filtre (p n : nat) :=
+  forall C : Set,
+  (forall P : nat -> Set,
+   (forall q : nat, P q -> natext p q) ->
+   (forall q : nat, P q -> P (S q)) -> P n -> C) -> C.
+
+(***************************************************************************)
+(* Fitrebuild: construction d'une preuve de Filtre(p,n)  		   *)
+(***************************************************************************)
+Lemma Filtrebuild :
+ forall (p n : nat) (P : nat -> Set),
+ (forall q : nat, P q -> natext p q) ->
+ (forall q : nat, P q -> P (S q)) -> P n -> Filtre p n.
+intros.
+red in |- *.
+intros.
+apply (H2 P); trivial with v62.
+Qed.
+
+(***************************************************************************)
+(* Filtrehd: construction d'une preuve de natext(p,n) a partir d'une 	   *)
+(* preuve de Filtre(n,p)						   *)
+(***************************************************************************)
+Lemma Filtrehd : forall p n : nat, Filtre p n -> natext p n.
+intros.
+apply H.
+auto with v62.
+Qed.
+Hint Immediate Filtrehd.
+
+(***************************************************************************)
+(* Filtre: construction d'une preuve de Filtre(p,Sn)	 a partir d'une	   *)
+(* preuve de Filtre(p,n)						   *)
+(***************************************************************************)
+Lemma Filtretl : forall p n : nat, Filtre p n -> Filtre p (S n).
+intros.
+apply H.
+intros.
+apply Filtrebuild with P; auto with v62.
+Qed.
+Hint Resolve Filtretl.
+
+
+(***************************************************************************)
+(* LEMME1: s'il existe r, 1<r<=q tq r divise p				   *)
+(*	   alors il existe aussi r', 1<r'<=Sq tq r' divise p (par ex r'=r) *)
+(***************************************************************************)
+Lemma lemme1 : forall q p : nat, divinf p q -> divinf p (S q).
+simple induction 1; intros.
+apply Bs with r; auto with v62.
+Qed.
+Hint Resolve lemme1.
+
+
+(***************************************************************************)
+(* SPECIFICATION DE Sift					   *)
+(* Pour tous n et p tels que p>=1, a partir d'une preuve de Filtre(p,n)	   *)
+(* on veut construire une preuve de Filtre(Sp,n)			   *)
+(***************************************************************************)
+
+Lemma Sift : forall p n : nat, 1 <= p -> Filtre p n -> Filtre (S p) n.
+intros.
+apply Filtrebuild with (Filtre p); auto with v62.
+intros; elim (Filtrehd p q); intros; auto with v62.
+elim (div_dec (S p) q); auto with v62.
+intros; apply Div.
+apply Bs with (S p); auto with v62.
+Qed.
+
+
+(***************************************************************************)
+(* SPECIFICATION DU CRIBLE D'ERATOSTHENE				   *)
+(* Il existe une propriete P de type nat->Set telle que:		   *)
+(* - on ait une preuve de P(1)						   *)
+(* - pour tout q une preuve de P(q) donne une preuve de P(Sq)		   *)
+(* - pour tout q une preuve de P(q) donne une preuve de natext(q,Sq) i.e   *)
+(*   une preuve de l'existence ou non d'un diviseur de Sq dans [1...q]	   *)
+(***************************************************************************)
+Definition Eratosthene :=
+  forall C : Set,
+  (forall P : nat -> Set,
+   (forall q : nat, P q -> natext q (S q)) ->
+   (forall q : nat, P q -> P (S q)) -> P 1 -> C) -> C.
+
+
+Lemma Eratobuild :
+ forall P : nat -> Set,
+ (forall q : nat, P q -> natext q (S q)) ->
+ (forall q : nat, P q -> P (S q)) -> P 1 -> Eratosthene.
+red in |- *; intros.
+apply (H2 P); auto with v62.
+Qed.
+
+
+(***************************************************************************)
+(* LEMME2: pour tout q et r, une preuve de l'existence d'un diviseur de Sq *)
+(*	   dans [1...q] et une preuve de l'existence ou non d'un diviseur  *)
+(*         de r dans [1...q]  donne une preuve de l'existence ou non de r  *)
+(*	   dans [1...Sq]  (preuve du lemme par cas)			   *)
+(***************************************************************************)
+
+Lemma lemme2 :
+ forall q r : nat, divinf (S q) q -> natext q r -> natext (S q) r.
+simple induction 2; auto with v62.
+Qed.
+Hint Resolve lemme2.
+
+
+(***************************************************************************)
+(* DEFINITION INDUCTIVE							   *)
+(* Pred(q) propriete de tout q>=1 dont on a une preuve de Filtre(q,Sq)	   *)
+(***************************************************************************)
+Inductive Pred (q : nat) : Set :=
+    Predintro : 1 <= q -> Filtre q (S q) -> Pred q.
+Hint Resolve Predintro.
+
+
+(***************************************************************************)
+(* PREUVE DE LA SPECIFICATION DU CRIBLE D'ERATOSTHENE			   *)
+(* le terme correspondant a la preuve est le terme de F-OMEGA realisant la *)
+(* specification							   *)
+(***************************************************************************)
+
+Theorem Crible : Eratosthene.
+apply Eratobuild with Pred.  (* Elimination du "il existe" ds Eratobuild *)
+intros; apply Filtrehd.
+elim H; trivial with v62.
+intros; elim H; intros.
+apply Predintro; auto with v62.
+elim (Filtrehd q (S q) f); intro.
+apply Filtretl.
+apply Sift; auto with v62.
+apply f; intros.
+apply Filtrebuild with P; auto with v62. 
+apply Predintro; auto with v62.
+apply Filtrebuild with (natext 1); auto with v62.
+Qed.
+
+
+(* $Id$ *)
---
+++ GfpStreams.v
@@ -0,0 +1,357 @@
+(* This program is free software; you can redistribute it and/or      *)
+(* modify it under the terms of the GNU Lesser General Public License *)
+(* as published by the Free Software Foundation; either version 2.1   *)
+(* of the License, or (at your option) any later version.             *)
+(*                                                                    *)
+(* This program is distributed in the hope that it will be useful,    *)
+(* but WITHOUT ANY WARRANTY; without even the implied warranty of     *)
+(* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      *)
+(* GNU General Public License for more details.                       *)
+(*                                                                    *)
+(* You should have received a copy of the GNU Lesser General Public   *)
+(* License along with this program; if not, write to the Free         *)
+(* Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA *)
+(* 02110-1301 USA                                                     *)
+
+
+(* Contribution to the Coq Library   V6.3 (July 1999)                    *)
+(****************************************************************************)
+(*                 The Calculus of Inductive Constructions                  *)
+(*                                                                          *)
+(*                                Projet Coq                                *)
+(*                                                                          *)
+(*                     INRIA                        ENS-CNRS                *)
+(*              Rocquencourt                        Lyon                    *)
+(*                                                                          *)
+(*                                 Coq V6.1                                 *)
+(*                               Oct 1st 1996                               *)
+(*                                                                          *)
+(****************************************************************************)
+(*                                Streams.v                                 *)
+(****************************************************************************)
+
+Require Import Arith.
+Require Import Eqdep.
+
+
+Section Streams_.
+
+Variable U : Set.
+
+Inductive Streams (A : U -> U -> Set) (m : U) : Set :=
+    build :
+      forall P : U -> Set,
+      (forall p : U, P p -> {q : U &  A p q &  P q}) -> P m -> Streams A m.
+
+Variable A : U -> U -> Set.
+
+Let streams := Streams A.
+
+Definition eq_streams := eq_dep U streams.
+
+Lemma eq_streams_intro : forall (m : U) (X : streams m), eq_streams m X m X.
+red in |- *; auto with v62.
+Qed.
+Hint Resolve eq_streams_intro.
+
+Lemma eq_streams_sym :
+ forall (m n : U) (X : streams m) (Y : streams n),
+ eq_streams m X n Y -> eq_streams n Y m X.
+exact (eq_dep_sym U streams).
+Qed.
+Hint Immediate eq_streams_sym.
+
+Lemma eq_streams_trans :
+ forall (m n p : U) (X : streams m) (Y : streams n) (Z : streams p),
+ eq_streams m X n Y -> eq_streams n Y p Z -> eq_streams m X p Z.
+exact (eq_dep_trans U streams).
+Qed.
+
+Lemma out : forall m : U, streams m -> {q : U &  A m q &  streams q}.
+simple induction 1; intros P s y.
+elim (s m); intros; trivial with v62.
+exists x; trivial with v62.
+red in |- *; apply build with P; auto with v62.
+Defined.
+
+Lemma hd : forall m : U, streams m -> U.
+intros; elim (out m H); intros.
+exact x.
+Defined.
+
+Lemma tail : forall (m : U) (X : streams m), streams (hd m X).
+intros; unfold hd in |- *; elim (out m X); auto with v62.
+Defined.
+
+Lemma out_hd_tail :
+ forall (m : U) (X : streams m),
+ {q : U &  A m q &  {Y : streams q | eq_streams q Y (hd m X) (tail m X)}}.
+intros; unfold hd, tail in |- *; elim (out m X).
+intros x a Y; exists x; trivial with v62.
+exists Y; trivial with v62.
+Defined.
+
+Lemma hd_nth : nat -> forall m : U, streams m -> U.
+simple induction 1; intros.
+exact m.
+apply (H0 (hd m H1) (tail m H1)).
+Defined.
+
+Lemma tail_nth :
+ forall (n : nat) (m : U) (X : streams m), streams (hd_nth n m X).
+simple induction n; simpl in |- *; intros; trivial with v62.
+(***
+Induction n; Simpl; Intros; Trivial with v62.
+Change (streams (hd_nth y (hd m X) (tail m X))); Trivial with v62.
+***)
+Defined.
+
+Lemma tail_plus :
+ forall (n k : nat) (m : U) (X : streams m),
+ eq_streams (hd_nth (n + k) m X) (tail_nth (n + k) m X)
+   (hd_nth k (hd_nth n m X) (tail_nth n m X))
+   (tail_nth k (hd_nth n m X) (tail_nth n m X)).
+simple induction n; simpl in |- *; trivial with v62.
+(***
+Induction n; Simpl; Trivial with v62.
+Intros.
+Change (eq_streams (hd_nth (plus y k) (hd m X) (tail m X))
+     (tail_nth (plus y k) (hd m X) (tail m X))
+     (hd_nth k (hd_nth y (hd m X) (tail m X))
+       (tail_nth y (hd m X) (tail m X)))
+     (tail_nth k (hd_nth y (hd m X) (tail m X))
+       (tail_nth y (hd m X) (tail m X)))); Trivial with v62.
+***)
+Qed.
+Hint Resolve tail_plus.
+
+Lemma specif : forall (m : U) (X : streams m), A m (hd m X).
+intros; unfold hd in |- *; elim (out m X); auto with v62.
+Qed.
+
+
+(*******************************************************)
+(*   Filter                                            *)
+(*******************************************************)
+
+Section Filter_.
+
+(* Load Between. *)
+
+Variable P Q : U -> Prop.
+
+(* R is decidable *)
+Hypothesis PQ_dec : forall x : U, {P x} + {Q x}.
+Hypothesis PQ_excl : forall x : U, P x -> ~ Q x.
+
+Hint Immediate PQ_excl.
+
+Variable B : U -> U -> Set.
+Hypothesis incl : forall x y : U, A x y -> B x y.
+Hypothesis trans_ARB : forall x y z : U, A x y -> P y -> B y z -> B x z.
+Hint Resolve incl.
+
+Section Stream_X.
+
+Variable x : U.
+Variable X : streams x.
+Let hdSX (k : nat) := hd_nth (S k) x X.
+
+Let tlSX (n : nat) : streams (hdSX n) := tail_nth (S n) x X.
+
+Definition P_nth_X (k : nat) : Prop := P (hdSX k).
+
+Definition Q_nth_X (k : nat) : Prop := Q (hdSX k).
+
+
+Inductive filter_specif (q : U) : Set :=
+    filter_intro :
+      B x q ->
+      Q q ->
+      forall (Y : streams q) (m : nat),
+      eq_streams q Y (hdSX m) (tlSX m) ->
+      between P_nth_X 0 m -> filter_specif q.
+
+End Stream_X.
+
+Section Properties.
+
+Variable x q : U.
+Variable X : streams x.
+Variable Y : streams q.
+Hypothesis Y_tlX : eq_streams q Y (hd x X) (tail x X).
+
+Lemma P_nth_X_O : P q -> P_nth_X x X 0.
+red in |- *; simpl in |- *; elim Y_tlX; auto with v62.
+(***
+Red; Change ((P q)->(P (hd x X))); Elim Y_tlX; Auto with v62.
+***)
+Qed.
+Hint Resolve P_nth_X_O.
+
+Lemma Q_nth_X_O : Q_nth_X x X 0 -> Q q.
+unfold Q_nth_X in |- *; simpl in |- *; elim Y_tlX; auto with v62.
+(***
+Unfold Q_nth_X; Change ((Q (hd x X))->(Q q)); Elim Y_tlX; Auto with v62.
+***)
+Qed.
+Hint Resolve Q_nth_X_O.
+
+Lemma event_Q_nth_O : eventually (Q_nth_X x X) 0 -> Q q.
+intros; apply Q_nth_X_O; apply event_O; trivial with v62.
+Qed.
+
+Lemma P_nth_X_S : forall m : nat, P_nth_X q Y m -> P_nth_X x X (S m).
+red in |- *; simpl in |- *; intros.
+elim Y_tlX; auto with v62.
+(***
+Red; Change ((m:nat)
+    (P_nth_X q Y m)
+     ->(P (hd_nth m (hd (hd x X) (tail x X)) (tail (hd x X) (tail x X))))).
+Intros; Elim Y_tlX; Auto with v62.
+***)
+Qed.
+Hint Resolve P_nth_X_S.
+
+Lemma Q_nth_X_S : forall m : nat, Q_nth_X x X (S m) -> Q_nth_X q Y m.
+unfold Q_nth_X in |- *; simpl in |- *.
+elim Y_tlX; auto with v62.
+(***
+Unfold Q_nth_X ; Change ((m:nat)
+    (Q (hd_nth m (hd (hd x X) (tail x X)) (tail (hd x X) (tail x X))))
+     ->(Q (hd_nth m (hd q Y) (tail q Y)))).
+Elim Y_tlX; Auto with v62.
+***)
+Qed.
+Hint Resolve Q_nth_X_S.
+
+Lemma event_Q_nth_X_tl :
+ forall l : nat,
+ ~ Q q -> eventually (Q_nth_X x X) (S l) -> eventually (Q_nth_X q Y) l.
+simple induction 2; red in |- *; intro k.
+pattern k in |- *; apply nat_case.
+intros; absurd (Q q); auto with v62.
+intros; exists m; auto with v62.
+Qed.
+
+Lemma between_P_nth_X_tl :
+ forall l : nat,
+ P q -> between (P_nth_X q Y) 0 l -> between (P_nth_X x X) 0 (S l).
+simple induction 2; auto with v62.
+Qed.
+
+End Properties.
+
+Lemma filter_step :
+ forall (n : nat) (x : U) (X : streams x),
+ eventually (Q_nth_X x X) n -> {q : U &  filter_specif x X q}.
+simple induction n; intros.
+(* case n=0 let q::Y = X *)
+elim (out_hd_tail x X); intros q Axq HY.
+elim HY; intros Y eqY.
+(* the solution is (q,Y,O) because 
+   (eventually (Q_nth_X x X) O)->(Q q)  *)
+exists q; apply filter_intro with Y 0; auto with v62.
+apply event_Q_nth_O with x X Y; trivial with v62.
+(* case n = (S y), let q::Y = X *)
+elim (out_hd_tail x X); intros q Axq HY.
+elim HY; intros Y eqY.
+(* the solution depends on (P q) or (Q q) *)
+elim (PQ_dec q); intro.
+(* case (P q), apply inductively the result to (q,Y) *)
+elim H with q Y.
+(* let q'::Z be the result for Y *)
+intros q' f'.
+elim f'; intros B' notR Z m eqm betm.
+(* the solution is (q',Z,(S m)) *)
+exists q'; apply filter_intro with Z (S m); trivial with v62.
+(* Proof of (B x q') *)
+apply trans_ARB with q; auto with v62.
+(* Proof of 
+     (eq_streams q' Z (hd_nth (S (S m)) x X) (tail_nth (S (S m)) x X)) *)
+change
+  (eq_streams q' Z (hd_nth (S m) (hd x X) (tail x X))
+     (tail_nth (S m) (hd x X) (tail x X))) in |- *.
+elim eqY; trivial with v62.
+apply between_P_nth_X_tl with q Y; trivial with v62.
+apply event_Q_nth_X_tl with x X; auto with v62.
+(* case (Q q), the solution is (q,O,Y) *)
+exists q; apply filter_intro with Y 0; auto with v62.
+Qed.
+
+Variable x : U.
+Variable X : streams x.
+
+Let hdX (k : nat) := hd_nth k x X.
+
+Let tlX (n : nat) : streams (hdX n) := tail_nth n x X.
+
+Hypothesis
+  remanent :
+    forall n : nat, {p : nat | eventually (Q_nth_X (hdX n) (tlX n)) p}.
+
+Inductive Filter_specif (p q : U) : Set :=
+    Filter_intro :
+      B p q ->
+      Q q ->
+      forall n m : nat,
+      hdX n = p ->
+      hdX (S m) = q -> between (P_nth_X x X) n m -> Filter_specif p q.
+
+Section Lemmes.
+Variable p : U.
+Variable Y : streams p.
+Variable n : nat.
+Hypothesis eqY : eq_streams (hdX n) (tlX n) p Y.
+
+Remark hd_nth_tail :
+ forall l : nat, hd_nth (S (n + l)) x X = hd_nth (S l) p Y.
+elim eqY; intro.
+replace (S (n + l)) with (n + S l); auto with v62.
+unfold hdX, tlX in |- *; elim (tail_plus n (S l) x X); auto with v62.
+Qed.
+
+Remark P_nth_X_tail : forall l : nat, P_nth_X p Y l -> P_nth_X x X (n + l).
+unfold P_nth_X in |- *.
+intro; elim hd_nth_tail; auto with v62.
+Qed.
+Hint Resolve P_nth_X_tail.
+
+Lemma between_between_tail :
+ forall m : nat, between (P_nth_X p Y) 0 m -> between (P_nth_X x X) n (n + m).
+simple induction 1; intros; auto with v62.
+replace (n + S l) with (S (n + l)); auto with v62.
+Qed.
+End Lemmes.
+
+Definition Filter_prop (p : U) :=
+  {Y : streams p &  {n : nat | eq_streams (hdX n) (tlX n) p Y}}.
+
+Theorem Filter : Streams Filter_specif x.
+apply build with Filter_prop.
+simple induction 1; intros Y HY.
+elim HY; intros n HYn.
+elim (remanent n); intros ln evln.
+elim (filter_step ln p Y).
+intros q fq; elim fq; intros Bq Rq Z m eqZm betZm.
+cut (eq_streams (hdX (n + S m)) (tlX (n + S m)) q Z).
+intro eqZ; exists q.
+apply Filter_intro with n (n + m); trivial with v62.
+elim HYn; trivial with v62.
+elim eqZ; replace (S (n + m)) with (n + S m); auto with v62.
+apply between_between_tail with p Y; trivial with v62.
+red in |- *; exists Z; exists (n + S m); trivial with v62.
+apply eq_streams_trans with (hd_nth (S m) p Y) (tail_nth (S m) p Y);
+ auto with v62.
+elim HYn.
+exact (tail_plus n (S m) x X).
+elim HYn; trivial with v62.
+(* Initial state : X *)
+red in |- *; exists X; exists 0; auto with v62.
+Qed.
+
+End Filter_.
+
+End Streams_.
+
+(* $Id$ *)
